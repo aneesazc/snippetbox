2982d49973668e13869f959eeac6bcc2758c8a4e
aneesazc <anees.azc@gmail.com>
2024-06-04 17:47:53 +0530

    add sql queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,33 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+
+	// "html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/aneesazc/snippetbox/internal/models"
 )
 
 // Change the signature of the home handler so it is defined as a method against
 // *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
-
-    files := []string{
-        "./ui/html/base.tmpl",
-        "./ui/html/partials/nav.tmpl",
-        "./ui/html/pages/home.tmpl",
-    }
-
-    ts, err := template.ParseFiles(files...)
+    
+    snippets, err := app.snippets.Latest()
     if err != nil {
         app.serverError(w, r, err)
         return
     }
 
-    err = ts.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        app.serverError(w, r, err)
+    for _, snippet := range snippets {
+        fmt.Fprintf(w, "%+v\n", snippet)
     }
+
+    // files := []string{
+    //     "./ui/html/base.tmpl",
+    //     "./ui/html/partials/nav.tmpl",
+    //     "./ui/html/pages/home.tmpl",
+    // }
+
+    // ts, err := template.ParseFiles(files...)
+    // if err != nil {
+    //     app.serverError(w, r, err)
+    //     return
+    // }
+
+    // err = ts.ExecuteTemplate(w, "base", nil)
+    // if err != nil {
+    //     app.serverError(w, r, err)
+    // }
 }
 
 // Change the signature of the snippetView handler so it is defined as a method
@@ -39,7 +53,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+    snippet, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            http.NotFound(w, r)
+        } else {
+            app.serverError(w, r, err)
+        }
+        return
+    }
+
+    // Write the snippet data as a plain-text HTTP response body.
+    fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
@@ -51,6 +76,17 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 // Change the signature of the snippetCreatePost handler so it is defined as a method
 // against *application.
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusCreated)
-    w.Write([]byte("Save a new snippet..."))
+    title := "O snail"
+    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+    expires := 7
+
+
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+        app.serverError(w, r, err)
+        return
+    }
+
+    // Redirect the user to the relevant page for the snippet.
+    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
